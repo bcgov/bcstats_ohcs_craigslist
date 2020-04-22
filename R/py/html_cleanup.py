9b1681b57e050865aa3bda889bf4a71543d1f69d
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-22 15:05:44 -0700

    update

--- R/py/html_cleanup.py
+++ R/py/html_cleanup.py
@@ -6,7 +6,7 @@ exec(open("py" + os.path.sep + "strtok.py").read())
 ''' finer-grained parsing and clean up, of html files produced by
      1) extract.cpp
      2) parse.py
-    
+
     examples:
         incl. price:    python3 html_cleanup.py parsed/5447094859
         without price:  python3 html_cleanup.py parsed/4760673195
@@ -28,10 +28,10 @@ def html_cleanup(fn):
     data = {}
     if not os.path.exists(fn):
         err("Error: could not find file: " + str(fn))
-    
+
     html = open(fn).read()  # open the data
     soup = BeautifulSoup(html, 'lxml')  # parse the data
-    
+
     # results from fp() get keyed w./ tag, minus symbol prefix \in {., #}
     tags = ['.price',
             '.housing',
@@ -40,19 +40,19 @@ def html_cleanup(fn):
             '#postingbody',
             '.notices',
             '.mapbox']
-    
+
     for s in tags:
         data[s[1: ]] = fp(soup, s)
-    
+
     data['postingbody'] = data['postingbody'].replace('<br/>','')
     data['postingbody'] = data['postingbody'].replace('<section id="postingbody">','')
     data['postingbody'] = data['postingbody'].replace('</section>','')
-    data['postingbody'] = " ".join(data['postingbody'].split())   
+    data['postingbody'] = " ".join(data['postingbody'].split())
     data['notices'] = data['notices'].replace('<ul class="notices">', '')
     data['notices'] = data['notices'].replace('<li>', '')
     data['notices'] = data['notices'].replace('</ul>', '')
     data['notices'] = data['notices'].replace('</li>', '')
-    
+
     # clean up price:
     try:
         # strip outer tags, remove $ sign
@@ -62,7 +62,7 @@ def html_cleanup(fn):
         p = p.strip('$')
         data['price'] = p
     except Exception: pass
-    
+
     # clean up housing
     try:
         # strip outer tags and remove $ sign
@@ -71,7 +71,7 @@ def html_cleanup(fn):
         h = strtok(h, '<')[0]
         data['housing'] = h
     except Exception: pass
-    
+
     # clean up housing
     data['title'] = ''
     try:
@@ -82,11 +82,11 @@ def html_cleanup(fn):
         del data['titletextonly']
         data['title'] = t
     except Exception: pass
-    
+
     # clean up attrgroup e.g.: ['<p class="attrgroup"><span><b>3</b>BR / <b>1.5</b>Ba</span> <span><b>1150</b>ft<sup>2</sup></span>']
     a = data['attrgroup'].strip()
     data['attrgroup'] = a
-    
+
     # get number of bedrooms
     bed = ''
     try:
@@ -115,7 +115,7 @@ def html_cleanup(fn):
             if len(a.split('Ba<')) < 2:
                 pass
             elif len(a.split('>Ba')) < 2:
-                # negative: e.g. parsed/4841847119 
+                # negative: e.g. parsed/4841847119
                 # positive: e.g. parsed/6134742515
                 x = a.split('Ba<')[0]
                 x = x.split('<b>')[1]
@@ -144,62 +144,62 @@ def html_cleanup(fn):
         data['movein_date'] = x
     except Exception:
         data['movein_date'] = ''
-    
+
     # clean up postingbody
     b = data['postingbody']
     try:
         b = b.split('<a class="showcontact"')[0].strip()
     except Exception: pass
     data['postingbody'] = b.replace('&amp;','&')
-    
+
     # clean up mapbox
     m = data['mapbox']
     accuracy, latitude, longitude, address, mapzoom = '', '', '', '', ''
-    
+
     # clean up data accuracy
     try:
         accuracy = m.split('data-accuracy="')[1]
         accuracy = accuracy.split('"')[0]
     except Exception: pass
     data['map_accuracy'] = accuracy
-    
+
     # clean up latitude
     try:
         latitude = m.split('data-latitude="')[1]
         latitude = latitude.split('"')[0]
     except Exception: pass
     data['map_latitude'] = latitude
-    
+
     # clean up longitude
     try:
         longitude = m.split('data-longitude="')[1]
         longitude = longitude.split('"')[0]
     except Exception: pass
     data['map_longitude'] = longitude
-    
+
     # clean up address
     try:
         address = m.split('<p class="mapaddress">')[1]
         address = address.split('<small>')[0]
     except Exception: pass
     data['map_address'] = address
-    
-    
+
+
     # clean up map url
     try:
         mapzoom = m.split('<a href="')[1].split(',')[2].split('"')[0]
     except Exception: pass
     data['map_zoom'] = mapzoom.strip('z')
-    
-    
+
+
     # cleanse strings of unfriendly characters (for csv)
     for d in data:
         data[d] = cleanse(data[d])
-    
+
     if False:
         for d in data:
             print(d, "\n", [data[d]], "\n")
-        
+
     # pragmatic programming: explicitly order the elements
 
     elems = ['price', 'bed', 'bath', 'title', 'map_accuracy', 'map_address', 'map_latitude', 'map_longitude', 'map_zoom', 'movein_date', 'notices', 'postingbody', 'attrgroup', 'mapbox', 'housing']
