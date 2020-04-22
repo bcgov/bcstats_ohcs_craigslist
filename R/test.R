0c25d0747e12c91bcf64c000c1fbc237dfb8f8e3
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-22 15:49:12 -0700

    update

--- R/test.R
+++ R/test.R
@@ -24,7 +24,7 @@ mod<-function(x, m){
 
 # source a cpp function
 src<-function(x){
-  cat(paste("src(\"", x, "\")\n", sep="")) # , quote=FALSE)
+  cat(paste("src(\"", x, "\")\n", sep=""))
   Rcpp::sourceCpp(x, cacheDir='tmp')
 }
 
