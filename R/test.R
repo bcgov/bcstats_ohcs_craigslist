6bb91fa93d4c2b3dec10dae491d4a35c34fb2e9d
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-22 19:40:50 -0700

    update

--- R/test.R
+++ R/test.R
@@ -5,76 +5,83 @@
 # 4) retain only non-redundant records
 # 5) remove intermed. files, after "join"
 
-meta_file <- "craigslist-bc-sublets-data-mar.csv"
-html_file <- "craigslist-sublet-data-bc-html-mar.csv"
-
 library(Rcpp) # install.packages("Rcpp")
 library(reticulate) # install.packages("reticulate")
 
-p_sep <- .Platform$file.sep  # platform specific path separator
+p_sep <- .Platform$file.sep # platform specific path separator
+
+mod<-function(x, m){
+  return(x - floor(x / m) * m)
+}
+
+err<-function(m){
+  cat(paste("Error:", m, "\n"))
+  quit()
+}
 
 pr<-function(x){
   # thanks to Sam Albers and Craig Hutton for helping solve this:
   print(paste(paste(deparse(substitute(x)), "="), x))
 }
 
-mod<-function(x, m){
-  return(x - floor(x / m) * m)
-}
-
-# source a cpp function
 src<-function(x){
+  # source a cpp function
   cat(paste("src(\"", x, "\")\n", sep=""))
   Rcpp::sourceCpp(x, cacheDir='tmp')
 }
 
-# test big-data resilient csv-file concatenation
-src("cpp/insist.cpp") # assertion
-src("cpp/csv_cat.cpp") # merge arbitrarily large csv: assert headers match, keep first header
+harmari_craigslist_parsing<-function(html_file, meta_file){
 
-print(paste("test", p_sep, "A.csv", sep=""))
-csv_cat(c(paste("test", p_sep, "A.csv", sep=""),
-paste("test", p_sep, "B.csv", sep=""),
-paste("test", p_sep, "C.csv", sep="")))
+  # c++ functionality    
+  src("cpp/lc.cpp") # wc -l analog to avoid R.utils dep
+  src("cpp/extract.cpp")
+  src("cpp/insist.cpp") # assertion
+  src("cpp/csv_cat.cpp") # merge arb. large csv: assert headers match, keep first hdr
+  src("cpp/find_start.cpp") # index an HTML nested in csv, find <HTML> byte start locs
 
-x <- read.csv(paste("test", p_sep, "C.csv", sep=""),
-header=TRUE)
-for(i in 0: 7){
-  a <- ceiling((i + 1)/ 2)
-  b <- b <- 1 + mod(i, 2)
-  insist(x[a, b] == i + 1)
+  # python functionality
+  cat("Note: if prompted to install Miniconda, please say yes!\n")
+  py_discover_config()
+  py_available(initialize=TRUE)
+  if(!py_available()){
+    err("python not initialized")
+  }
+  import_from_path("py")
+  source_python("py/join.py")
+  source_python("py/html_parse.py")
+
+  test_csv_cat<-function(){
+    # test big-data resilient csv-file concatenation
+  
+    print(paste("test", p_sep, "A.csv", sep=""))
+    csv_cat(c(paste("test", p_sep, "A.csv", sep=""),
+    paste("test", p_sep, "B.csv", sep=""),
+    paste("test", p_sep, "C.csv", sep="")))
+
+    x <- read.csv(paste("test", p_sep, "C.csv", sep=""), header=TRUE)
+    for(i in 0: 7){
+      a <- ceiling((i + 1)/ 2)
+      b <- b <- 1 + mod(i, 2)
+      insist(x[a, b] == i + 1)
+    }
+  }
+  test_csv_cat()
+      
+  # test file indexing
+  tag_file <-paste(html_file, "_tag", sep="")
+  if(!file.exists(tag_file)){
+    find_start(html_file)
+  }
+
+  extract(html_file)  # HTML file extraction
+
+  # count number of records from "meta" file
+  n_records <-lc(meta_file)
+  html_parse(paste(html_file, n_records, sep=","))
+  join(paste(html_file, meta_file, n_records, sep=','))
 }
 
-# test file indexing
-src("cpp/find_start.cpp") # index a file with HTML nested inside csv, finding locations of <HTML> start tags
-tag_file <-paste(html_file, "_tag", sep="")
-if(!file.exists(tag_file)){
-  find_start(html_file)
-}
 
-# test HTML file extraction
-src("cpp/extract.cpp")
-if(FALSE){
-  extract(html_file)
-}
-print("extract html complete.", quote=FALSE)
-
-# count number of records from "meta" file
-src("cpp/lc.cpp") # wc -l analog
-n_records <-lc(meta_file)
-print(n_records)
-
-# test parsing, python!
-print("if you're prompted to install Miniconda, please say yes")
-py_discover_config()
-py_available(initialize=TRUE)
-if(!py_available()){
-  print("Error: python not initialized", quote=FALSE)
-  quit()
-}
-import_from_path("py")
-source_python("py/html_parse.py")
-html_parse(paste(html_file, n_records, sep=","))
-
-source_python("py/join.py")
-join(paste(html_file, meta_file, n_records, sep=','))
+meta_file <- "craigslist-bc-sublets-data-mar.csv"
+html_file <- "craigslist-sublet-data-bc-html-mar.csv"
+harmari_craigslist_parsing(html_file, meta_file)
