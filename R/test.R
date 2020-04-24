b4c1597c1983faaa7c4bc17853ccb2570e9891d5
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-24 11:26:51 -0700

    update

--- R/test.R
+++ R/test.R
@@ -161,6 +161,10 @@ match_infiles<-function(in_dir){
   }
 
   # could hypothetically match on ID but we'll match on name and warn
+
+  # at this point, need to restrict string chunks to length 3, if not numeric
+
+  # might also need to adapt metric to handle potentially varying number of chunks (try it on all data files with original names, and see if it works!!!)
   for(i in 1:length(html)){
     s<-html[i]
     x<-chunks(s)
