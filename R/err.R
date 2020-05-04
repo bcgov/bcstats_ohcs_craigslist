d2f26684656fc748a2bbcb5f0216425ed72a3482
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 15:27:26 -0700

    update

--- R/err.R
+++ R/err.R
@@ -4,8 +4,9 @@ err <- function(msg){
 }
 
 # include other files if their member functions not yet def'd
+c("run.R", "setup.R")
 for(f in Sys.glob("*.R")){
-  if(f != parent.frame(2)$ofile && !(f %in% c("run.R", "setup.R"))){
+  if(f != parent.frame(2)$ofile && !(f %in% scripts)){
     if(!exists(strsplit(f, "\\.")[[1]][1], inherits=TRUE)) source(f)
   }
 }
