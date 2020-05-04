61d129412ee446660aed7b612947343adac822b1
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 15:26:31 -0700

    update

--- R/run.R
+++ R/run.R
@@ -1,63 +1,19 @@
-## test.R:
-## windows intructions: from cmd.exe:
-## Rscript test.R
+## Rscript run.R ## todo: check for nonunique records
 
-## todo: check for nonunique records
-
-# source function files
-files <- Sys.glob("*.R")
+# include other files if their member functions not yet def'd
 my_fn <- parent.frame(2)$ofile
 scripts <- c("run.R", "setup.R")
-for(f in files){
-  if(f != my_fn && !(f %in% scripts)){
-    source(f)
+for(f in Sys.glob("*.R")){
+  if(! is.null(my_fn)) if(f == my_fn) next
+  if(!(f %in% scripts)){
+    if(!exists(strsplit(f, "\\.")[[1]][1], inherits=TRUE)) source(f)
   }
 }
 
-args = commandArgs(trailingOnly=TRUE)
-if(length(args)==0){
-  # stop("test.R [input directory name]");
-}
-
-err <- function(msg){
-  cat(paste("Error:", msg, "\n", sep=""))
-  quit()
-}
-
-wait_return <- function(){
-  cat('press [return] to continue')
-  return(scan("stdin", character(), nlines=1, quiet=TRUE))
-}
-
-pkg_reqd<-c("Rcpp", "reticulate")
-if(FALSE){
-  to_install<-pkg_reqd[!(pkg_reqd %in% installed.packages()[,"Package"])]
-  if(length(to_install)){
-    cat("required packages to install:\n")
-    print(to_install)
-    cat("press return to install required packages:\n")
-    cat("** NOTE: if this doesn't work, please re-run the script with administrator / super-user privileges..\n")
-    wait_return()
-    install.packages(to_install)
-    err("install complete: exiting, please restart program\n")
-  }
-}
-library(Rcpp) # install.packages("Rcpp")
-library(reticulate) # install.packages("reticulate")
+library(Rcpp)
+library(reticulate)
 use_condaenv("r-reticulate")
-if(FALSE){
-
-  conda_create("r-reticulate")
-  conda_activate("r-reticulate")
-  use_condaenv("r-reticulate")
-  cat("checking that python libraries are installed..\n")
-  conda_install("r-reticulate", "bs4") # py_install("bs4")
-  conda_install("r-reticulate", "html5lib") # py_install("html5lib")
-  conda_install("r-reticulate", "lxml") # py_install("lxml")
-  cat("got past the python libraries installation\n")
-}
-
-p_sep <- .Platform$file.sep # platform specific path separator
+p_sep <- .Platform$file.sep # platform specific path sep
 
 rmrf<-function(d){
   cat(paste("rm -rf ", d, "\n", sep=""))
