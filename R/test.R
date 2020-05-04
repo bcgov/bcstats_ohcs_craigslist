a925abc6752359f82bd2521cd948a992cb54b4dd
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 08:50:40 -0700

    update

--- R/test.R
+++ R/test.R
@@ -21,24 +21,32 @@ wait_return <- function(){
 }
 
 pkg_reqd<-c("Rcpp", "reticulate")
-to_install<-pkg_reqd[!(pkg_reqd %in% installed.packages()[,"Package"])]
-if(length(to_install)){
-  cat("required packages to install:\n")
-  print(to_install)
-  cat("press return to install required packages:\n")
-  cat("** NOTE: if this doesn't work, please re-run the script with administrator / super-user privileges..\n")
-  wait_return()
-  install.packages(to_install)
-  err("install complete: exiting, please restart program\n")
+if(FALSE){
+  to_install<-pkg_reqd[!(pkg_reqd %in% installed.packages()[,"Package"])]
+  if(length(to_install)){
+    cat("required packages to install:\n")
+    print(to_install)
+    cat("press return to install required packages:\n")
+    cat("** NOTE: if this doesn't work, please re-run the script with administrator / super-user privileges..\n")
+    wait_return()
+    install.packages(to_install)
+    err("install complete: exiting, please restart program\n")
+  }
 }
-
 library(Rcpp) # install.packages("Rcpp")
 library(reticulate) # install.packages("reticulate")
-cat("checking that python libraries are installed..\n")
-py_install("bs4")
-py_install("html5lib")
-py_install("lxml")
-cat("got past the python libraries installation\n")
+use_condaenv("r-reticulate")
+if(FALSE){
+
+  conda_create("r-reticulate")
+  conda_activate("r-reticulate")
+  use_condaenv("r-reticulate")
+  cat("checking that python libraries are installed..\n")
+  conda_install("r-reticulate", "bs4") # py_install("bs4")
+  conda_install("r-reticulate", "html5lib") # py_install("html5lib")
+  conda_install("r-reticulate", "lxml") # py_install("lxml")
+  cat("got past the python libraries installation\n")
+}
 
 p_sep <- .Platform$file.sep # platform specific path separator
 
@@ -65,7 +73,6 @@ src<-function(x){
 ## parse craigslist data as supplied by Harmari, inc.
 harmari_craigslist_parsing<-function(html_file, meta_file){
   cat("harmari_craigslist_parsing", html_file, meta_file, "\n", sep=",")
-  wait_return()
 
   join_file = paste(meta_file, "_join.csv", sep="")
 
@@ -250,6 +257,7 @@ match_infiles<-function(in_dir){
     cat(paste("harmari_craigslist_parsing(", html_file, ",", meta_file, ")\n"), sep='')
   }
   cat("N.B. if you wish to abort, please press ctrl-c (and then return). If not, please just press return.\n")
+
   wait_return()
 
   join_files = character(0)
