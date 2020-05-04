40de3d75767f5d58c9214aaa6c6619edf9090bfa
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 12:42:01 -0700

    update

--- R/py/file_sizes.py
+++ R/py/file_sizes.py
@@ -1,10 +1,14 @@
-file_names = ['craigslist-bc-sublets-data-mar.csv', 'craigslist-sublet-data-bc-html-mar.csv', 'craigslist-apa-data-bc.csv', 'craigslist-sublet-data-bc.csv', 'craigslist-apa-data-bc-html-othermeta.csv', 'craigslist-sublet-data-bc-html-othermeta.csv', 'craigslist-apa-data-bc-html-jan.csv', 'craigslist-apa-data-bc-html-sep.csv', 'craigslist-bc-apa-data-jan.csv', 'craigslist-sublet-data-bc-html-jan.csv', 'craigslist-bc-sublet-data-sep.csv', 'craigslist-bc-sublet-data-jan.csv', 'craigslist-bc-apa-data-sep.csv', 'craigslist-sublet-data-bc-html-sep.csv']
-file_sizes = ['540821', '337123080', '301100188', '15806712', '22868906488', '1207390218', '3277267614', '1913701273', '35371689', '115374402', '727855', '1196955', '21663494', '65148136']
+file_names = ['craigslist-bc-sublets-data-mar.csv', 'craigslist-sublet-data-bc-html-mar.csv', 'craigslist-apa-data-bc.csv', 'craigslist-sublet-data-bc.csv', 'craigslist-apa-data-bc-html-othermeta.csv', 'craigslist-sublet-data-bc-html-othermeta.csv', 'craigslist-apa-data-bc-html-jan.csv', 'craigslist-apa-data-bc-html-sep.csv', 'craigslist-bc-apa-data-jan.csv', 'craigslist-sublet-data-bc-html-jan.csv', 'craigslist-bc-sublet-data-sep.csv', 'craigslist-bc-sublet-data-jan.csv', 'craigslist-bc-apa-data-sep.csv', 'craigslist-sublet-data-bc-html-sep.csv', 'craigslist-bc-apartment-data-apr.csv', 'craigslist-bc-sublets-data-apr.csv', 'craigslist-sublet-data-bc-html-apr.csv', 'craigslist-apa-data-bc-html-apr.csv']
+file_sizes = ['540821', '337123080', '301100188', '15806712', '22868906488', '1207390218', '3277267614', '1913701273', '35371689', '115374402', '727855', '1196955', '21663494', '65148136', '6931814', '218584', '308603553', '648129837']
 
 import os
 import sys
-lines = os.popen("ls -latr *.csv").readlines()
 
+if len(file_names) != len(file_sizes):
+    print("Data entry error: array lengths don't match")
+    sys.exit(1)
+
+lines = os.popen("ls -latr *.csv").readlines()
 file_names_update, file_sizes_update = [], []
 
 for line in lines:
@@ -23,8 +27,6 @@ for line in lines:
             file_names_update.append(fn)
             file_sizes_update.append(sz)
 
-print("file_names_update", file_names_update)
-print("file_sizes_update", file_sizes_update)
-
-
-    
+if len(file_names_update) > 0:
+    print("file_names_update", file_names_update)
+    print("file_sizes_update", file_sizes_update)  
