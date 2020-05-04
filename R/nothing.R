802985d20d557fc02a2cffe2534a3b7f256ce89e
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 14:41:03 -0700

    update

---
+++ R/nothing.R
@@ -0,0 +1,17 @@
+nothing<-function(){
+  print("nothing function()")
+}
+
+# include other files if the function in them with same name, not yet defined
+files <- Sys.glob("*.R")
+my_fn <- parent.frame(2)$ofile
+scripts <- c("run.R", "setup.R")
+for(f in files){
+  if(f != my_fn && !(f %in% scripts)){
+    print(paste("from:", my_fn, "to:", f))
+    fn <- strsplit(f, "\\.")[[1]][1] # primary function name
+    if(!exists(fn, inherits=TRUE)){
+      source(f)
+    }
+  }
+}
