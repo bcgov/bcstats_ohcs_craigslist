1f04ec1fcb83bdd2793ee21970e9d440ca2408a3
ashlinrichardson <richardson.ashlin@gmail.com>
2020-05-04 14:43:36 -0700

    update

--- R/nothing.R
+++ R/nothing.R
@@ -8,7 +8,6 @@ my_fn <- parent.frame(2)$ofile
 scripts <- c("run.R", "setup.R")
 for(f in files){
   if(f != my_fn && !(f %in% scripts)){
-    print(paste("from:", my_fn, "to:", f))
     fn <- strsplit(f, "\\.")[[1]][1] # primary function name
     if(!exists(fn, inherits=TRUE)){
       source(f)
