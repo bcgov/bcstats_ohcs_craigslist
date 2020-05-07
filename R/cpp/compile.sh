5c1fadd9649937e8b7b3d937b80ded9285c7162a
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-05-06 22:10:51 -0700

    update

--- R/cpp/compile.sh
+++ R/cpp/compile.sh
@@ -1,3 +1,4 @@
-g++ -O3 csv_slice.cpp misc.cpp -o csv_slice.exe
+g++ -O3 csv_slice.cpp  misc.cpp -o csv_slice.exe
 g++ -O3 csv_select.cpp misc.cpp -o csv_select.exe
 g++ -O3 csv_unique.cpp misc.cpp -o csv_unique.exe
+g++ -O3 snip.cpp       misc.cpp -o snip.exe
