a30a0931faf70cbb9a9b13036f05727837c9cf17
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-05-09 21:59:15 -0700

    update

--- R/csv_vselect.py
+++ R/csv_vselect.py
@@ -1,4 +1,11 @@
+# this program called by confirm_null.py
 # 20200508 like csv_select.cpp except doesn't parse the target into fields: parses into lines only
+# python3 csv_vselect.py craigslist-bc-sublets-data-mar.csv_join.csv_null.csv_slice.csv id craigslist-sublet-data-bc-html-mar.csv DOCTYPE > craigslist-sublet-data-bc-html-mar.csv_select.csv
+'''
+  45775 craigslist-apa-data-bc-html-othermeta.csv_select.csv
+  13877 craigslist-sublet-data-bc-html-apr.csv_select.csv
+  13931 craigslist-sublet-data-bc-html-mar.csv_select.csv
+'''
 import os; exists = os.path.exists
 import sys; args = sys.argv
 import csv
@@ -31,21 +38,30 @@ if hdr != [sf]: err("expected ['" + sf + "']")
 idx = set([x[0] for x in lines])
 # print(idx)
 
-lines = open(args[3]).readlines()
-hdr = lines[0]
-print(hdr.strip(), end="")
-if corroborant is None:
-    for i in range(0, len(lines)):
-        found, line = False, lines[i]
+
+f, ci = open(args[3]), -1
+fs = os.stat(args[3]).st_size
+
+line = f.readline()
+print(line.strip())
+
+while True:
+    found, line = False, f.readline()
+    if not line:
+        break
+    ci += 1
+
+    if corroborant is None:
         for j in idx:
             if j in line:
                 found = True
-        if found: print('\n' + line.strip(), end="")
-else:
-    for i in range(0, len(lines)):
-        found, line = False, lines[i]
+    else:
         if corroborant in line:
             for j in idx:
                 if j in line:
                     found = True
-        if found: print('\n' + line.strip(), end="")
+    if found:
+        print('\n' + line.strip(), end="")
+
+    if ci % 100000 == 0:
+        print("%" + str(round(100. * f.tell() / fs, 2)), file=sys.stderr)
