1644f63bdef30b7cc84e89fc0abd10e9d58ed718
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-06-11 13:12:16 -0700

    update

--- R/file_size_check.py
+++ R/file_size_check.py
@@ -9,9 +9,16 @@ file_names = ['craigslist-apa-data-bc-html-mar.csv', 'craigslist-bc-apartment-da
 
 file_sizes = [1313723416, 14167108, 540821, 337123080, 301100188, 15806712, 22868906488, 1207390218, 3277267614, 1913701273, 35371689, 115374402, 727855, 1196955, 21663494, 65148136, 6931814, 218584, 308603553, 648129837, 8114544, 179446, 763264057, 17573827]
 
-print("file,bytes expected,bytes")
+hdr = False
+
 for i in range(0, len(file_names)):
     f, fs, f_s = file_names[i], str(file_sizes[i]), None
     f_s = str(os.stat(f).st_size if os.path.exists(f) else 'N/A')
-    #if fs != f_s:
-    print(','.join([f, fs, f_s]))
+    if fs != f_s:
+        if not hdr:
+            print("file,bytes expected,bytes")
+            hdr = True
+        print(','.join([f, fs, f_s]))
+
+if not hdr:
+    print("(GOOD) All files present and of expected size")
