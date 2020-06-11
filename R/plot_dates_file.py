c15a9fc601c5849328216ad8d3063b5b618bcc39
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-06-11 13:32:34 -0700

    update

---
+++ R/plot_dates_file.py
@@ -0,0 +1,18 @@
+import os; exists = os.path.exists
+import sys; args = sys.argv
+
+def err(m):
+    print("Error:", m); sys.exit(1)
+
+def run(c):
+    print(c)
+    if os.system(c) != 0:
+        err("\tcommand failed")
+
+f = args[1]
+if not exists(f): err("could not find input csv file: " + f)
+if not exists('tmp'): os.mkdir('tmp')
+if not exists('cpp/csv_split.exe'): err("need to compile cpp/csv_split.cpp")
+run(' '.join(['cp', f, 'tmp']))
+run('cpp/csv_split.exe tmp/' + f)
+run('python3 py/plot_dates.py ' + 'tmp/' + f + '_postDate' + ' ' + f)
